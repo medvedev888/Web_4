05736b51234ec9f8c0d1ee53ff7f4717330ac9f0
vladislavmedvedev <EnoT3581@mail.ru>
2024-12-08 23:38:29 +0300

    added UserAlreadyExistException.java and UserNotFoundException.java;

---
+++ src/main/java/me/vladislav/web_4/exceptions/UserAlreadyExistException.java
@@ -0,0 +1,11 @@
+package me.vladislav.web_4.exceptions;
+
+public class UserAlreadyExistException extends RuntimeException {
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/me/vladislav/web_4/exceptions/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package me.vladislav.web_4.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
