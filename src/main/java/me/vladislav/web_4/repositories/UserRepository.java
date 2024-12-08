9bf988b9932dbd49cf2e36d4c8a35f4c16cf8575
vladislavmedvedev <EnoT3581@mail.ru>
2024-12-08 23:38:55 +0300

    added UserRepository.java;

---
+++ src/main/java/me/vladislav/web_4/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package me.vladislav.web_4.repositories;
+
+import me.vladislav.web_4.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByLogin(String login);
+}
